597fec6929381c91bc5d63052fdc070c046e6cff
Tingfeng Xia <tingfeng.xia@outlook.com>
2020-02-24 14:28:09 -0500

    static serve

---
+++ build/precache-manifest.ebbc00c2ceb4a507624043b43aa53375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e6b6c74af45b963f9ca273ece84708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c910b2014fb90b7ab771",
+    "url": "/static/css/main.ff35b52f.chunk.css"
+  },
+  {
+    "revision": "a421f52bf3e582272479",
+    "url": "/static/js/2.d010bdb1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d010bdb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c910b2014fb90b7ab771",
+    "url": "/static/js/main.234d93d4.chunk.js"
+  },
+  {
+    "revision": "06d5d3e7ff39a6a70d5d",
+    "url": "/static/js/runtime-main.7da11464.js"
+  }
+]);
